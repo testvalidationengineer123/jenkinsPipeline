bec8de15d47b6d1fc5622ca2f6d577ca604a3bf6
lahad <testvalidationengineer123@gmail.com>
2020-05-30 18:20:34 +0200

    add Jenkinsfile 6

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,7 +9,7 @@ pipeline{
 			steps {
 			
 				withMaven(maven: 'maven_3_6_3') {
-					sh 'mvn clean install -DnavigateurUtilise=chrome'
+					sh 'mvn clean install'
 				}
 			}
 		}
--- src/test/java/com/lahad/runners/Runner.java
+++ src/test/java/com/lahad/runners/Runner.java
@@ -30,10 +30,10 @@ public class Runner {
 	@BeforeClass
 	public static void setUp() {
 		String navigateur = System.getProperty("navigateurTest");
-		if("chrome".equals(navigateur)) {		
+//		if("chrome".equals(navigateur)) {		
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\lahad\\Desktop\\tools\\webdrivers\\chromedriver_83.0.4103.39\\chromedriver.exe");
 			driver = new ChromeDriver();
-		}
+//		}
 	}
 	
 	@AfterClass
